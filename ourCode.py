64720a715b69bd50a5c0f64345b4ae1d9ee96402
Guillaume Vanel <guillaume.vanel@insa-lyon.fr>
2019-10-04 09:28:42 +0200

    out in a ttxt file

--- ourCode.py
+++ ourCode.py
@@ -7,4 +7,8 @@ Ceci est un script temporaire.
 
 import numpy as np
 import os
-os.system("python3 start_simulation.py params.ini")
+os.system("python3 start_simulation.py params.ini > out.txt")
+with open("out.txt", "r") as out:
+    for line in out:
+        print(line)
+        
